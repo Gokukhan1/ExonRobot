d6f6938cca11820fb1981af957a5b03449c43ced
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-07-29 21:52:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,22 +8,22 @@ load_dotenv()
 class Config(object):
     LOGGER = True
 
-    API_ID = int(getenv("API_ID", 6))
-    API_HASH = getenv("API_HASH", None)
+    API_ID = int(getenv("API_ID", "23255238")
+    API_HASH = getenv("API_HASH", "009e3d8c1bdc89d5387cdd8fd182ec15")
     ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
     SPAMWATCH_API = None
-    TOKEN = getenv("TOKEN", None)
-    OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1001819078701"))
+    TOKEN = getenv("TOKEN", "6898485942:AAHztBpjqvu2nNHPrynApKpAJxbhU33Dfrg")
+    OWNER_ID = int(getenv("OWNER_ID", "7078181502")
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "Xeno_Kakarot")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "lolpagalokigc")
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1002009280180"))
     MONGO_URI = getenv(
         "MONGO_DB_URI",
         "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority",
     )
     DB_NAME = getenv("DB_NAME", "ExonRobot")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
-    DATABASE_URL = getenv("DATABASE_URL", None)
+    DATABASE_URL = getenv("DATABASE_URL", "postgres://oocekooc:W2w0GdYOHNwvqKh047VMGjhHq_Xlb2sS@hansken.db.elephantsql.com/oocekooc")
 
     # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
     if DATABASE_URL.startswith("postgres://"):
